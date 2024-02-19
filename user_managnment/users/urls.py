3439b8057d5c5bcc57a4dd158ec0e7a1d2588732
derrickkimtai <derrickmoio92@gmail.com>
2024-02-19 12:56:21 +0300

    django learning

--- user_managnment/users/urls.py
+++ user_managnment/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include  # Import include here
 from users.views import dashboard
+from users.views import register
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),  # Include the auth URLs properly
     path("dashboard/", dashboard, name="dashboard"),
+    path("register/", register, name="register"),
 ]
